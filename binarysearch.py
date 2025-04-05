22fcc929842857b1aa1d529fab297e65deb61aa9
BobbySmurf <135199516+BobbySmurf@users.noreply.github.com>
2025-04-04 22:15:39 -0500

    123

---
+++ binarysearch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def binarySearch(self, arr, n, k):
+        low = 0
+        high = n - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == k:
+                return mid
+            elif arr[mid] < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
